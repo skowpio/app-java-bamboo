a99c3ab5ee0c96b3dc11c3a3508b098ffe0e159a
kkrzysiu <kpwkowalcz@gmail.com>
2017-07-11 19:48:44 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-INSTALL_DIR="/opt/app-bamboo/custom_deploy/install_dir"
+INSTALL_DIR="/opt/app-bamboo/install"
 WORK_DIR="/tmp/target"
 POM_VERSION=$(cat $WORK_DIR/app-java-bamboo-version.properties)
 PACKAGE_PATH="${WORK_DIR}/app-java-bamboo-$POM_VERSION-dist.zip"
 
 mkdir -p ${INSTALL_DIR}
 
-#supervisorctl stop app-bamboo-custom-deploy
-
 unzip -q -o ${PACKAGE_PATH} -d ${INSTALL_DIR}
+
+if [ -f ${INSTALL_DIR}/current/stop.sh ]
+then
+chmod 0750 ${INSTALL_DIR}/current/stop.sh
+${INSTALL_DIR}/current/stop.sh
+fi
 
 rm -f ${INSTALL_DIR}/current
 ln -s ${INSTALL_DIR}/app-java-bamboo-${POM_VERSION} ${INSTALL_DIR}/current
 
-#supervisorctl start app-bamboo-custom-deploy
+chmod 0750 ${INSTALL_DIR}/current/start.sh
+chmod 0750 ${INSTALL_DIR}/current/stop.sh
+
+${INSTALL_DIR}/current/start.sh $1
