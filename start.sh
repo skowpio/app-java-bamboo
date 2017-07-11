285d69a30918a026a1636a2bf458a68fa419a0be
kkrzysiu <kpwkowalcz@gmail.com>
2017-07-11 19:54:33 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-bamboo/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
+echo $! > /opt/app-bamboo/app-bamboo.pid
