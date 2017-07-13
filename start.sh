577c8c851f09a952c6d82da554970c5d9dad3c43
skowpio <skowpio@gmail.com>
2017-07-13 11:31:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
+/opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-bamboo/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
+echo $! > /opt/app-bamboo/app-bamboo.pid
 
-is_run=$(ps x | grep -v grep | grep -c java)
-
-if [ $is_run -eq 0 ]; then
-  /opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-bamboo/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
-  echo $! > /opt/app-bamboo/app-bamboo.pid
-fi
